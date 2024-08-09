8c60fb4b40770e7c6d99d1cb23c5ca13e8c97f8e
factorialmap <marcelo.carvalho.anjos@hotmail.com>
2024-08-08 21:45:17 -0300

    create a logistic regression model

--- ml_mdl_recession_prob.py
+++ ml_mdl_recession_prob.py
@@ -3,6 +3,9 @@
 from fredapi import Fred
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 #insert api_key from fred
 fred = Fred(api_key='insert api_key')
@@ -42,5 +45,33 @@ data_rec = data_t10y3m.merge(
 data_rec.to_csv("data_rec.csv")
 
 
+#split data into a training and testing sets
+X = data_rec[["10y3m"]]
+y = data_rec[["recession"]]
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, 
+random_state=42)
+
+#create a logistic regression model
+mdl_log_reg_rec = LogisticRegression()
+
+#train the model in the training data
+mdl_log_reg_rec.fit(X_train, y_train)
+
+#make predictions on the testing data
+y_pred = mdl_log_reg_rec.predict(X_test)
+
+#evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+accuracy
+
+print("Accuracy:", accuracy)
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
 
 
+#predict new data
+new_data = pd.DataFrame({"10y3m":[1,0.5,-0.5]})
+
+new_pred = mdl_log_reg_rec.predict(new_data)
+new_pred
